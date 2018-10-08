2ee8cc9cb28176b74d5c54e2a869b7c83756b6fa
Fabian Affolter <mail@fabian-affolter.ch>
2018-10-08 07:16:11 +0200

    Add DB helper

---
+++ penin/helpers/__init__.py
@@ -0,0 +1,23 @@
+"""Helpers for PenIn."""
+import os
+
+from cement.utils import fs
+from tinydb import TinyDB
+
+
+def extend_tinydb(app):
+    """Add support for TinyDB."""
+    app.log.error("hhhhhhhhhhhhhhhhhh")
+    app.log.info('Extending PenIn with TinyDB')
+    db_file = app.config.get('penin', 'db_file')
+
+    # Ensure that we expand the full path
+    db_file = fs.abspath(db_file)
+    app.log.info('TinyDB database file is: {}'.format(db_file))
+
+    # Ensure our parent directory exists
+    db_dir = os.path.dirname(db_file)
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+
+    app.extend('db', TinyDB(db_file))
