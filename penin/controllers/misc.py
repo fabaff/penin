7b209b475376dd5b94197e46f9a072d63532ec1a
Fabian Affolter <mail@fabian-affolter.ch>
2019-04-25 00:09:54 +0200

    dd support for Base64 string manipulation (HTTP Basic auth) (#2)

--- penin/controllers/misc.py
+++ penin/controllers/misc.py
@@ -1,16 +1,18 @@
-"""Support for retrieving details about IP addresses in use."""
+"""Support for various helper commands."""
+import base64
+
 from cement import Controller, ex
 
 from penin.core import get_host_details, create_hash
 
 ALGORITHMS = [
-    'md5',
-    'sha1',
-    'sha256',
-    'sha384',
-    'sha512',
-    'ripemd160',
-    'whirlpool',
+    "md5",
+    "sha1",
+    "sha256",
+    "sha384",
+    "sha512",
+    "ripemd160",
+    "whirlpool",
 ]
 
 
@@ -24,9 +26,7 @@ class Misc(Controller):
         stacked_type = "nested"
         stacked_on = "base"
 
-    @ex(
-        help="retrieve the details about the host",
-    )
+    @ex(help="retrieve the details about the host")
     def host(self):
         """Retrieve details about the host."""
         result = get_host_details()
@@ -40,22 +40,50 @@ class Misc(Controller):
 
     @ex(
         help="create a hash of the given string",
-        arguments=[(["string"], {"help": "string to hash"}),
+        arguments=[
+            (["string"], {"help": "string to hash"}),
             (
-            ["-a", "--algorithm"],
-            {
-                "help": "algorithm to use",
-                "default": 'sha256',
-                "choices": ALGORITHMS,
-            },
-        ),
-
-
-                   ],
+                ["-a", "--algorithm"],
+                {
+                    "help": "algorithm to use",
+                    "default": "sha256",
+                    "choices": ALGORITHMS,
+                },
+            ),
+        ],
     )
     def hash(self):
         """Create hash of a given string."""
 
         result = create_hash(self.app.pargs.string, self.app.pargs.algorithm)
+        data = {"result": result}
+        self.app.render(data, "default.jinja2")
+
+    @ex(
+        help="handles the encoding/decoding of a string",
+        arguments=[
+            (["string"], {"help": "string to hash"}),
+            (
+                ["-e", "--encode"],
+                {"help": "encode the string", "action": "store_true"},
+            ),
+            (
+                ["-d", "--decode"],
+                {"help": "decode a string", "action": "store_true"},
+            ),
+        ],
+    )
+    def base64(self):
+        """Retrieve details about the host."""
+        if self.app.pargs.decode:
+            result = base64.b64decode(
+                self.app.pargs.string).decode("utf-8", "ignore")
+        if self.app.pargs.encode:
+            result = base64.b64encode(
+                self.app.pargs.string.encode()).decode("utf-8", "ignore")
+        if result is None:
+            self.app.log.error("Unable to handle the string")
+            return
+
         data = {"result": result}
         self.app.render(data, "default.jinja2")
