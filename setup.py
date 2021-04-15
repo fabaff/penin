6a93984357b4478907746225bfadea0bfc8b681c
Fabian Affolter <mail@fabian-affolter.ch>
2021-04-15 21:51:19 +0200

    iFOrmatting with black

--- setup.py
+++ setup.py
@@ -5,25 +5,23 @@ from penin.core.version import get_version
 
 VERSION = get_version()
 
-readme_file = open('README.md', 'r')
+readme_file = open("README.md", "r")
 LONG_DESCRIPTION = readme_file.read()
 readme_file.close()
 
 setup(
-    name='penin',
+    name="penin",
     version=VERSION,
-    description='Information gathering and penetration testing framework',
+    description="Information gathering and penetration testing framework",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    url='https://github.com/fabaff/penin',
-    license='Apache 2.0',
-    packages=find_packages(exclude=['ez_setup', 'tests*']),
-    package_data={'penin': ['templates/*']},
+    long_description_content_type="text/markdown",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    url="https://github.com/fabaff/penin",
+    license="Apache 2.0",
+    packages=find_packages(exclude=["ez_setup", "tests*"]),
+    package_data={"penin": ["templates/*"]},
     include_package_data=True,
-    install_requires=['cement', 'pyyaml', 'colorlog', 'jinja2', 'tinydb'],
-    entry_points={
-        'console_scripts': ['penin = penin.main:main']
-    },
+    install_requires=["cement", "pyyaml", "colorlog", "jinja2", "tinydb"],
+    entry_points={"console_scripts": ["penin = penin.main:main"]},
 )
